31dfd27bc212d8d17f4c19b5721f29cdd6fad986
Rafa Mel <melrcm@gmx.net>
2019-01-12 06:52:30 +0100

    Improves system clean (logs containers)

--- src/clean/system.ts
+++ src/clean/system.ts
@@ -1,5 +1,30 @@
-// docker volume prune
-// docker network prune
-// docker image prune -a
+import chalk from 'chalk';
+import exec from '~/utils/exec';
+import containerLs from '~/utils/container-ls';
+import logger from 'loglevel';
 
-// docker container ls --all --format "{{json .}}" // -> Get JSON output and format at will
+export default async function cleanSystem({ force }: { force: boolean }) {
+  logger.setLevel('trace');
+  logger.info(chalk.yellow('\nStarting docker system-wide cleanup'));
+
+  // @ts-ignore
+  await exec('docker', ['volume', 'prune', force && '--force'].filter(Boolean));
+
+  await exec(
+    'docker',
+    // @ts-ignore
+    ['network', 'prune', force && '--force'].filter(Boolean)
+  );
+
+  await exec(
+    'docker',
+    // @ts-ignore
+    ['image', 'prune', '--all', force && '--force'].filter(Boolean)
+  );
+
+  // Show running container information
+  logger.info('\n' + chalk.green('Docker containers in system:'));
+  (await containerLs({ all: true })).map(({ ID, Status, Names }) => {
+    logger.info(`${ID}\t${Status.split(' ')[0]}\t   ${Names}`);
+  });
+}
