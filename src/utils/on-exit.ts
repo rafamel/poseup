a69bbce5a5f43a782b4b829a8f900a1fbfdf98e6
Rafa Mel <melrcm@gmx.net>
2019-01-14 17:52:40 +0100

    Fixes on-exit

--- src/utils/on-exit.ts
+++ src/utils/on-exit.ts
@@ -1,6 +1,6 @@
 import cleanup from 'node-cleanup';
 import logger from 'loglevel';
-import { series } from 'promist';
+import { deferred, status } from 'promist';
 
 type QueueCallback = () => Promise<void> | void;
 
@@ -8,40 +8,45 @@ type QueueCallback = () => Promise<void> | void;
 const queue: Array<{ task: string; cb: QueueCallback }> = [];
 
 let isAttached = false;
+let hasCleanup = false;
 let hasRun = false;
-let isDone = false;
-
+const p = status(deferred());
 export function attach() {
   if (isAttached) return;
   isAttached = true;
   cleanup((code, signal) => {
-    if (hasRun || isDone) return isDone;
-    run().then(() => process.kill(process.pid, signal || 'SIGTERM'));
+    if (p.status === 'resolved') return true;
+    if (hasCleanup) return false;
+    hasCleanup = true;
+    run();
+    p.then(() => {
+      process.kill(process.pid, signal || 'SIGTERM');
+    });
     return false;
   });
 }
 
 export async function run() {
-  if (hasRun) return;
+  if (hasRun) return p;
   hasRun = true;
-  if (!queue.length) return (isDone = true);
 
   logger.info('\nPreparing exit');
-
-  await series.each(queue, async ({ task, cb }) => {
-    logger.debug('  ' + task);
+  while (queue.length) {
+    // @ts-ignore
+    const { task, cb } = queue.shift();
+    logger.info('  ' + task);
     try {
-      return cb();
+      await cb();
     } catch (e) {
       logger.error(e);
     }
-  });
+  }
 
   logger.info('Done');
-  isDone = true;
+  p.resolve(null);
 }
 
 export default function onExit(taskName: string, cb: QueueCallback) {
-  queue.push({ task: taskName, cb });
+  queue.unshift({ task: taskName, cb });
   return cb;
 }
