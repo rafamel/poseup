e240d5823d18e4ae57a0e9b683a4d8c4c6a7f0ac
Rafa Mel <melrcm@gmx.net>
2019-01-29 15:31:47 +0100

    feat(compose): implements clean and stop after compose exits

--- src/bin/poseup-compose.ts
+++ src/bin/poseup-compose.ts
@@ -11,11 +11,13 @@ program
   .name('poseup compose')
   .usage('[options] -- [dockerArgs]')
   .description('Runs docker-compose')
+  .option('-w, --write <path>', 'Path to write a resulting docker compose file')
+  .option('-s, --stop', 'Stop all services on exit')
+  .option('-c, --clean', 'Run clean on exit')
+  .option('--dry', "Don't run docker compose - write only docker compose file")
   .option('-f, --file <path>', 'Path for config file [js,json,yml]')
   .option('-e, --env <env>', 'Environment for config file should be run on')
   .option('-d, --dir <dir>', 'Project directory [cwd by default]')
-  .option('-w, --write <path>', 'Path to write a resulting docker compose file')
-  .option('--dry', "Don't run docker compose [only write docker compose file]")
   .option('--log <level>', 'Logging level')
   .parse(argv);
 
@@ -26,8 +28,11 @@ compose({
   write: program.write,
   log: program.log,
   args: composeArgs,
-  dry: !!program.dry
+  dry: !!program.dry,
+  clean: !!program.clean,
+  stop: !!program.stop
 }).catch(async (e) => {
+  // eslint-disable-next-line no-console
   console.log('\n' + chalk.red('Error: ') + e.message);
   process.exit(1);
 });
