6b9e277e60cf08d2f11ef726941b354e984fcb3e
Rafa Mel <melrcm@gmx.net>
2019-01-30 18:07:25 +0100

    refactor: updates exits to latest and improves initialize()

--- src/utils/add.ts
+++ src/utils/add.ts
@@ -3,25 +3,18 @@ import { add as _add } from 'exits';
 
 // Sets priority (larger runs first)
 export enum ADD_TYPES {
+  START_LOG = 10,
   STOP = 3,
   CLEAN = 2,
   // File removal must go last in order for them to still exist on clean/stop
-  REMOVE_TEMP_FILES = 1,
-  END_LOG = -9999 - 1
+  REMOVE_TEMP_FILES = 1
 }
 
-let any = false;
 export default function add(
   type: ADD_TYPES,
   message: string,
   cb: (type: string, arg: any, context: any) => any | Promise<any>
 ): () => void {
-  if (!any) {
-    any = true;
-    _add(() => logger.info('\nPreparing exit'), 9999);
-    _add(() => logger.info('Done'), -9999);
-  }
-
   return _add((...args) => {
     logger.info('  ' + message);
     return cb(...args);
--- src/utils/initialize.ts
+++ src/utils/initialize.ts
@@ -1,5 +1,5 @@
 import has from '~/utils/has';
-import { options, attach, add, resolver } from 'exits';
+import { options, attach, add, resolver, on } from 'exits';
 import { IPoseup } from '~/types';
 import { ADD_TYPES } from './add';
 import chalk from 'chalk';
@@ -19,6 +19,22 @@ export default async function initialize({
   environment,
   log
 }: IPoseup): Promise<void> {
+  // Attachment must go first (other potential errors must be catched)
+  attach();
+  options({
+    spawned: {
+      signals: 'none',
+      wait: 'all'
+    },
+    resolver(type, arg) {
+      if (type === 'exception' || type === 'rejection') {
+        type = 'exit';
+        arg = 1;
+      }
+      return resolver(type, arg);
+    }
+  });
+
   if (log) setLevel(log);
   if (environment) process.env.NODE_ENV = environment;
 
@@ -34,43 +50,33 @@ export default async function initialize({
     throw Error(`${name} not available`);
   }
 
-  options({
-    spawned: {
-      signals: 'none',
-      wait: 'all'
-    },
-    resolver(type, arg) {
-      if (type === 'exception' || type === 'rejection') {
-        type = 'exit';
-        arg = 1;
-      }
-      return resolver(type, arg);
-    }
-  });
-
-  attach();
-
   if (!added) {
     added = true;
-    add((type, arg) => {
-      switch (type) {
-        case 'exception':
-        case 'rejection':
-          // @ts-ignore
-          logger.error('\n' + chalk.red('Error: ') + arg.message);
-          break;
-        case 'exit':
-          if (arg !== 0) {
-            logger.error(
-              '\n' +
-                chalk.red('Error: ') +
-                `Spawned process exited with code ${arg}`
-            );
-          }
-          break;
-        default:
-          break;
+    on('triggered', () => logger.info('\nWaiting on termination'));
+    add(() => logger.info('\nPreparing exit'), ADD_TYPES.START_LOG);
+    on('done', (getState) => {
+      logger.info('Done');
+      const { triggered } = getState();
+      if (triggered) {
+        switch (triggered.type) {
+          case 'exception':
+          case 'rejection':
+            // @ts-ignore
+            logger.error('\n' + chalk.red('Error: ') + triggered.arg.message);
+            break;
+          case 'exit':
+            if (triggered.arg !== 0) {
+              logger.error(
+                '\n' +
+                  chalk.red('Error: ') +
+                  `Spawned process exited with code ${triggered.arg}`
+              );
+            }
+            break;
+          default:
+            break;
+        }
       }
-    }, ADD_TYPES.END_LOG);
+    });
   }
 }
