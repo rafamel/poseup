25e90d003dc676fe0d5867ea4d99e130674e6fb7
Rafa Mel <melrcm@gmx.net>
2019-03-27 08:13:15 +0100

    test(utils): adds tests for writeYaml()

---
+++ test/utils/write-yaml.test.ts
@@ -0,0 +1,80 @@
+import _add from '../../src/utils/add';
+import writeYaml from '../../src/utils/write-yaml';
+import ensureError from '../../src/utils/ensure-error';
+import { TMP_DIR } from '../../src/constants';
+import fs from 'fs-extra';
+import path from 'path';
+import uuid from 'uuid/v4';
+import pify from 'pify';
+
+const add: any = _add;
+jest.mock('../../src/utils/add');
+
+const file = path.join(TMP_DIR, 'foo.yml');
+const nested = path.join(TMP_DIR, uuid(), 'foo.yml');
+const files = [file];
+afterAll(async () => {
+  for (let file of files) {
+    try {
+      await pify(fs.unlink)(file);
+    } catch (e) {}
+  }
+  await pify(fs.remove)(path.parse(nested).dir);
+});
+
+const read = (file: string): Promise<string> =>
+  pify(fs.readFile)(file)
+    .then((x) => String(x).trim())
+    .catch((e) => Promise.reject(ensureError(e)));
+
+describe(`explicit path`, () => {
+  test(`creates file`, async () => {
+    await expect(
+      writeYaml({
+        path: file,
+        data: 'foo'
+      })
+    ).resolves.toBe(file);
+    await expect(read(file)).resolves.toBe('foo');
+  });
+  test(`creates dir if not non existent`, async () => {
+    await expect(
+      writeYaml({
+        path: nested,
+        data: 'foo'
+      })
+    ).resolves.toBe(nested);
+    await expect(read(nested)).resolves.toBe('foo');
+  });
+  test(`doesn't call add`, async () => {
+    add.mockClear();
+
+    await expect(
+      writeYaml({
+        path: file,
+        data: 'foo'
+      })
+    ).resolves.toBe(file);
+    expect(add).not.toHaveBeenCalled();
+  });
+});
+describe(`random path`, () => {
+  test(`creates file`, async () => {
+    const file = await writeYaml({ data: 'foo' }).catch(() => 'fail');
+    files.push(file);
+
+    await expect(read(file)).resolves.toBe('foo');
+  });
+  test(`calls add w/ file removing callback`, async () => {
+    add.mockClear();
+    let rm = (): void => {};
+    add.mockImplementationOnce((...args: any[]) => (rm = args[2]));
+    const file = await writeYaml({ data: 'foo' }).catch(() => 'fail');
+    files.push(file);
+
+    await expect(read(file)).resolves.toBe('foo');
+    expect(add).toHaveBeenCalledTimes(1);
+    await expect(rm()).resolves.not.toBeInstanceOf(Error);
+    await expect(read(file)).rejects.toBeInstanceOf(Error);
+  });
+});
