c8493542bf058b8270e00636ebda58f70a2ecb35
Rafa Mel <melrcm@gmx.net>
2019-04-15 16:46:08 +0200

    feat(bin): properly uses attach() and isAttached() on binary entry point

--- src/bin/poseup.ts
+++ src/bin/poseup.ts
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
 import main from './main';
+import attach, { isAttached } from '~/utils/attach';
 import { error } from 'cli-belt';
-import { state, terminate } from 'exits';
+import { terminate } from 'exits';
 import logger from '~/utils/logger';
 
-main(process.argv.slice(2)).catch((err) => {
-  return state().attached.rejection
-    ? terminate('rejection', err)
-    : error(err, { exit: 1, debug: true, logger });
-});
+attach()
+  .then(() => main(process.argv.slice(2)))
+  .catch((err) => {
+    return isAttached()
+      ? terminate('rejection', err)
+      : error(err, { exit: 1, debug: true, logger });
+  });
