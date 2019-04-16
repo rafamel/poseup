6e4086d4513643cbdc7f343124792534b72c46d7
Rafa Mel <melrcm@gmx.net>
2019-04-16 08:21:42 +0200

    test(bin/main): adds tests for clean

---
+++ test/bin/main/clean.test.ts
@@ -0,0 +1,73 @@
+import clean from '~/bin/main/clean';
+import { clean as command } from '~/commands';
+import { IOfType } from '~/types';
+import argv from 'string-argv';
+
+jest.mock('~/commands');
+const mocks: IOfType<jest.Mock<any, any>> = {
+  clean: command,
+  // eslint-disable-next-line no-console
+  console: console.log = jest.fn()
+} as any;
+beforeEach(() => Object.values(mocks).forEach((mock) => mock.mockClear()));
+
+test(`shows help`, async () => {
+  await expect(clean(argv('--help'))).resolves.toBeUndefined();
+  await expect(clean(argv('-h'))).resolves.toBeUndefined();
+  await expect(clean(argv('-e dev --help'))).resolves.toBeUndefined();
+
+  expect(mocks.console).toHaveBeenCalledTimes(3);
+  expect(mocks.clean).not.toHaveBeenCalled();
+});
+test(`fails on positional`, async () => {
+  await expect(clean(argv('pos'))).rejects.toBeInstanceOf(Error);
+  await expect(clean(argv('-e dev pos'))).rejects.toBeInstanceOf(Error);
+  await expect(clean(argv('pos -e dev'))).rejects.toBeInstanceOf(Error);
+
+  expect(mocks.clean).not.toHaveBeenCalled();
+});
+test(`fails on unknown arg`, async () => {
+  await expect(clean(argv('-e dev -p'))).rejects.toBeInstanceOf(Error);
+
+  expect(mocks.clean).not.toHaveBeenCalled();
+});
+test(`suceeds w/ no args`, async () => {
+  await expect(clean([])).resolves.toBeUndefined();
+
+  expect(mocks.clean).toHaveBeenCalledTimes(1);
+  expect(mocks.clean).toHaveBeenCalledWith({
+    volumes: undefined,
+    file: undefined,
+    directory: undefined,
+    environment: undefined,
+    log: undefined
+  });
+});
+test(`passes all args`, async () => {
+  const args = argv(
+    '--volumes --env dev --dir ./foo/bar --file foo/bar.js --log debug'
+  );
+  await expect(clean(args)).resolves.toBeUndefined();
+
+  expect(mocks.clean).toHaveBeenCalledTimes(1);
+  expect(mocks.clean).toHaveBeenCalledWith({
+    volumes: true,
+    file: 'foo/bar.js',
+    directory: './foo/bar',
+    environment: 'dev',
+    log: 'debug'
+  });
+});
+test(`passes all args as aliases`, async () => {
+  const args = argv('-v -e dev -d ./foo/bar -f foo/bar.js');
+  await expect(clean(args)).resolves.toBeUndefined();
+
+  expect(mocks.clean).toHaveBeenCalledTimes(1);
+  expect(mocks.clean).toHaveBeenCalledWith({
+    volumes: true,
+    file: 'foo/bar.js',
+    directory: './foo/bar',
+    environment: 'dev',
+    log: undefined
+  });
+});
