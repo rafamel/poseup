62b457ca0ef122228c134d4faced4f4366ace648
Rafa Mel <melrcm@gmx.net>
2019-04-16 19:20:16 +0200

    refactor(utils/rejects): improves type definition

--- src/utils/rejects.ts
+++ src/utils/rejects.ts
@@ -3,7 +3,7 @@ export const DEFAULT_MESSAGE = 'An error occurred';
 export default rejects;
 
 function rejects(err: any, condition: true): Promise<never>;
-function rejects(err: any, condition: boolean): Promise<void>;
+function rejects(err: any, condition: boolean | undefined): Promise<void>;
 function rejects(err: any): Promise<never>;
 async function rejects(err: any, condition: boolean = true): Promise<void> {
   if (!condition) return;
