701233662667a858ba50d500497c1b12a6ad7eee
Rafa Mel <melrcm@gmx.net>
2019-05-14 19:27:01 +0200

    test(lifecycle): adds tests for add

---
+++ test/lifecycle/add.test.ts
@@ -0,0 +1,42 @@
+import add, { ADD_TYPES } from '~/lifecycle/add';
+import manager from '~/lifecycle/manager';
+import { IOfType } from '~/types';
+import { wait } from 'promist';
+import { setLevel } from '~/utils/logger';
+
+setLevel('silent');
+jest.mock('~/lifecycle/manager');
+
+const mocks: IOfType<jest.Mock> = {
+  add: manager.add
+} as any;
+
+beforeEach(() => Object.values(mocks).forEach((mock) => mock.mockClear()));
+
+test(`calls manager.add w/ fn and type`, () => {
+  expect(add(ADD_TYPES.START_LOG, '', () => {})).toBeUndefined();
+  expect(mocks.add).toHaveBeenCalledTimes(1);
+  expect(typeof mocks.add.mock.calls[0][0]).toBe('function');
+  expect(mocks.add.mock.calls[0][1]).toBe(ADD_TYPES.START_LOG);
+});
+test(`fn executes and is awaited for but nevers errors out`, async () => {
+  const fn = jest.fn().mockImplementation(() => wait(250));
+  add(ADD_TYPES.START_LOG, '', fn);
+  const cb = mocks.add.mock.calls[0][0];
+  expect(fn).not.toHaveBeenCalled();
+
+  const before = Date.now();
+  await expect(cb()).resolves.toBeUndefined();
+  expect(fn).toHaveBeenCalledTimes(1);
+  expect(Date.now() - before).toBeGreaterThanOrEqual(250);
+
+  fn.mockImplementationOnce(() => Promise.reject(Error()));
+  await expect(cb()).resolves.toBeUndefined();
+  expect(fn).toHaveBeenCalledTimes(2);
+
+  fn.mockImplementationOnce(() => {
+    throw Error();
+  });
+  await expect(cb()).resolves.toBeUndefined();
+  expect(fn).toHaveBeenCalledTimes(3);
+});
