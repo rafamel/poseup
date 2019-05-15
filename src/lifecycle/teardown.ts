661cade83ecd424da5e00195d9c36903c5d6a9bf
Rafa Mel <melrcm@gmx.net>
2019-05-15 14:05:24 +0200

    fix(lifecycle/teardown): takes into account order of addition for equal types

--- src/lifecycle/teardown.ts
+++ src/lifecycle/teardown.ts
@@ -1,9 +1,15 @@
 import manager from './manager';
+import { ADD_TYPES } from './add';
 
 export default async function teardown(): Promise<void> {
   const fns = manager
     .get()
-    .sort((a, b) => b[1] - a[1])
+    .map(
+      ([fn, type], index): [() => any | Promise<any>, ADD_TYPES, number] => {
+        return [fn, type, index];
+      }
+    )
+    .sort((a, b) => b[1] - a[1] || b[2] - a[2])
     .map((x) => x[0]);
 
   for (let fn of fns) {
